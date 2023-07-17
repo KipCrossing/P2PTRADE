ca502769d4cebfb2bbf76c4ac0cdb62117b859ea
kipling <kip.crossing@gmail.com>
2023-07-17 15:19:40 +1000

    refactor: remove default fluff

---
+++ src/routes/pay/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Pay</title>
+	<meta name="description" content="Payments Page" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Make a payment</h1>
+
+	<p>
+		payment details...
+	</p>
+
+</div>
