b3541c7885e45533a455f0631815e8f25418d52b
kipling <kip.crossing@gmail.com>
2023-07-17 15:20:06 +1000

    feat: implements setProvider to connect metamask

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import { onMount } from 'svelte';
+import {
+  connected,
+  web3,
+  selectedAccount,
+  chainId,
+  chainData
+} from 'svelte-web3'
+
+import { defaultEvmStores } from 'svelte-web3'
+
+onMount(() => {
+  // add a test to return in SSR context
+  defaultEvmStores.setProvider()
+})
+
 </script>
 
 <svelte:head>
@@ -11,21 +24,13 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+		Traiding app
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
+		Example <strong>strong text</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
@@ -41,19 +46,4 @@
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
