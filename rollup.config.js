33625707a1fd0dd7bb32401bc8079adceefe9088
kipling <kip.crossing@gmail.com>
2023-07-24 14:01:32 +1000

    build: dont emit css into another file

--- rollup.config.js
+++ rollup.config.js
@@ -48,10 +48,11 @@ export default {
         // enable run-time checks when not in production
         dev: !production,
       },
+      emitCss: false,
     }),
     // we'll extract any component CSS out into
     // a separate file - better for performance
-    css({ output: "bundle.css" }),
+    // css({ output: "bundle.css" }),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
