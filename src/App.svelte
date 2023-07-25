a9a5ad2e54f3ea4bcac9d3ab744dda609c430c56
kipling <kip.crossing@gmail.com>
2023-07-25 15:13:39 +1000

    feat: use all list as default and create in mordal

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-  import { Affix, Container, SvelteUIProvider } from "@svelteuidev/core";
+  import {
+    Affix,
+    Container,
+    SvelteUIProvider,
+    Button,
+    Modal,
+  } from "@svelteuidev/core";
   import AccountInfo from "./components/AccountInfo.svelte";
   import CreateEscrowComp from "./components/CreateEscrowComp.svelte";
   import ViewEscrowComp from "./components/ViewEscrowComp.svelte";
+  import AllEscrowComp from "./components/AllEscrowComp.svelte";
 
   const urlParams = new URLSearchParams(window.location.search);
   const escrowID: string | null = urlParams.get("escrowID") || null;
+
+  let opened = false;
+
+  function closeModal() {
+    opened = false;
+  }
 </script>
 
 <svelte:head>
@@ -14,6 +27,9 @@
 </svelte:head>
 
 <SvelteUIProvider>
+  <Modal {opened} on:close={closeModal} title="Create Escrow">
+    <CreateEscrowComp />
+  </Modal>
   <Container size="xs" override={{ px: "xs" }}>
     <h1>Trading app</h1>
     <Affix position={{ top: 20, right: 20 }}>
@@ -23,7 +39,9 @@
     {#if escrowID}
       <ViewEscrowComp {escrowID} />
     {:else}
-      <CreateEscrowComp />
+      <Button color="green" on:click={() => (opened = true)}>New escrow</Button>
+      <br />
+      <AllEscrowComp />
     {/if}
   </Container>
 </SvelteUIProvider>
