423f61d2ca3cd53d375a04539c679a7cf689ff01
kipling <kip.crossing@gmail.com>
2023-07-31 09:20:32 +1000

    feat: get merchantEscrows from the contract

--- src/components/AllEscrowComp.svelte
+++ src/components/AllEscrowComp.svelte
@@ -5,20 +5,67 @@
   import { escrowList, getEscrowList } from "../utils/storage";
   import { onMount } from "svelte";
   import { getAccount } from "../utils/getAccount";
+  import { ethers } from "ethers";
+  import { contractAddress } from "../utils/consts";
+  import { StakedEscrow__factory, type StakedEscrow } from "../types";
+  import { writable, type Writable } from "svelte/store";
 
-  let allEscrowList: EscrowInfo[] = [];
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const abi = StakedEscrow__factory.abi;
+
+  const stakedEscrowContract = new ethers.Contract(
+    contractAddress,
+    abi,
+    provider
+  ) as any as StakedEscrow;
+
+  type Escrow = {
+    buyer: string;
+    merchant: string;
+    amount: bigint;
+    details: string;
+    isDead: boolean;
+    complete: boolean;
+  };
+
+  let allEscrowList: Writable<(Escrow & { escrowId: number })[]> = writable([]);
+
+  async function getEscrowInfo(escrowID: bigint) {
+    const escrow: Escrow = await stakedEscrowContract.escrows(escrowID);
+    return escrow;
+  }
 
   onMount(() => {
-    getAccount().then((acc) => {
-      // console.log(acc);
-      allEscrowList = getEscrowList(acc);
-      // console.log(allEscrowList);
+    let allEscrowListTemp: (Escrow & { escrowId: number })[] = [];
+    getMerchantEscrows().then((escrowIDs: readonly bigint[]) => {
+      const escrowIDsList: bigint[] = [];
+      escrowIDs.forEach((escrowID) => {
+        escrowIDsList.push(escrowID);
+      });
+      escrowIDsList.reverse().forEach((escrowID) => {
+        console.log(escrowID);
+        getEscrowInfo(escrowID).then((escrow) => {
+          allEscrowListTemp.push({ ...escrow, escrowId: Number(escrowID) });
+          allEscrowList.set(allEscrowListTemp);
+        });
+      });
     });
   });
+
+  async function getMerchantEscrows() {
+    const signer = provider.getSigner();
+
+    const address = (await signer).address;
+    console.log(address);
+
+    console.log("getting...");
+    const escrowIDs = await stakedEscrowContract.getMerchantEscrows(address);
+    return escrowIDs;
+  }
 </script>
 
 <Container size="xs" override={{ px: "xs" }}>
-  {#each $escrowList as { escrowId, amount, details }, i}
+  {#each $allEscrowList as { escrowId, amount, details }, i}
     <li>
       <Card shadow="sm" padding="lg">
         <h3>Escrow {escrowId}</h3>
