dbdf620bac63ce7ad7bbe700e51b26893627a1d0
kipling <kip.crossing@gmail.com>
2023-07-30 15:33:54 +1000

    refactor: update types for new contract

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { StakedEscrow } from "./StakedEscrow";
+export * as factories from "./factories";
+export { StakedEscrow__factory } from "./factories/StakedEscrow__factory";
