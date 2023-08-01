9fea3f34d1537a4274a7c62c7fd7acef9b38dfb5
kipling <kip.crossing@gmail.com>
2023-08-01 18:00:36 +1000

    feat: useContract

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,12 +1,31 @@
-import { createPublicClient, http, createWalletClient, custom } from "viem";
+import {
+  createPublicClient,
+  http,
+  createWalletClient,
+  custom,
+  getContract,
+} from "viem";
 import { mainnet, sepolia } from "viem/chains";
+import { contractAddress } from "./consts";
+import { StakedEscrow__factory } from "../types";
 
 export const publicClient = createPublicClient({
   chain: sepolia,
   transport: custom(window.ethereum),
 });
 
-export const walletClient = createWalletClient({
-  chain: sepolia,
-  transport: custom(window.ethereum),
-});
+export function useContract(account: `0x${string}`) {
+  const walletClient = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: custom(window.ethereum),
+  });
+
+  const contract = getContract({
+    address: contractAddress,
+    abi: StakedEscrow__factory.abi,
+    publicClient,
+    walletClient,
+  });
+  return contract;
+}
