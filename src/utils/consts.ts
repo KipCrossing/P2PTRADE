c0952612db21f8e3fa60d29d4c02c4e8c04f945f
kipling <kip.crossing@gmail.com>
2023-08-12 14:05:38 +1000

    feat: add type to  consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,4 @@
-export const contractAddress = "0x3e013f301A2E09546ab3f1a5354bC53C51A6341E";
-export const NO_ONE = "0x0000000000000000000000000000000000000000";
+export const contractAddress: `0x${string}` =
+  "0x3e013f301A2E09546ab3f1a5354bC53C51A6341E";
+export const NO_ONEs: `0x${string}` =
+  "0x0000000000000000000000000000000000000000";
